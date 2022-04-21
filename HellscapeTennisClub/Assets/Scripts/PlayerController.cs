6a3f0652ff7a916e319769d8a2057dc0292f189a
colwynb <103963872+colwynb@users.noreply.github.com>
2022-04-20 22:04:27 -0400

    Squashed commit of the following:
    commit 926a82219845b4bd5d95b3cb4e00ef04c3006350
    Merge: 79e6ed8 3aa995c
    Author: bweers <75956719+bweers@users.noreply.github.com>
    Date:   Wed Apr 20 18:01:41 2022 -0400
    
        Merge pull request #7 from bweers/brian
    
        Brian
    
    commit 3aa995c6d45c900dcbc8afc929363b676482eedd
    Author: bweers <75956719+bweers@users.noreply.github.com>
    Date:   Wed Apr 20 18:00:34 2022 -0400
    
        Charge system implementation
    
    commit 49bd092a06e791127d911912d7e2b9860fbbf62a
    Author: bweers <75956719+bweers@users.noreply.github.com>
    Date:   Wed Apr 20 16:44:00 2022 -0400
    
        Tennis Ball Creation
    
    commit 79e6ed882891c7f99bd06a85d948b8ec77845a1d
    Merge: 09e179b 56903af
    Author: colwynb <103963872+colwynb@users.noreply.github.com>
    Date:   Wed Apr 20 16:41:46 2022 -0400
    
        Merge pull request #5 from bweers/colwyn
    
        Added attack inputs

--- HellscapeTennisClub/Assets/Scripts/PlayerController.cs
+++ HellscapeTennisClub/Assets/Scripts/PlayerController.cs
@@ -4,35 +4,75 @@ using UnityEngine;
  
 public class PlayerController : MonoBehaviour
 {
-  // private PlayerInputActions playerInput;
   private Rigidbody2D rb;
-  public float moveSpeed = 8f;
   private Camera cam;
+  private float moveSpeed = 8f, timer = 0f;
+  public float chargeDelay = .1f;
   private Vector2 moveDirection;
+  private int charge, maxCharge = 100;
+  private bool leftMouseDown;
  
+    private void Start()
+    {
+        StartCoroutine(ChargeTimer());
+        GameObject ballSpawnPointObject = GameObject.Find("BallSpawnPoint");
+    }
+    
    void Awake()
    {
-      //  playerInput = new PlayerInputActions();
        rb = GetComponent<Rigidbody2D>();
        cam = Camera.main;
    }
 
    void Update()
    {
-     ProcessInputs();
+        ProcessInputs();
    }
 
    void FixedUpdate()
    {
-      Move();
+        Move();
 
-      // LookAtMouse
-       Vector3 mouse = Input.mousePosition;
-       Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
-       Vector2 offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
-       float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
-       transform.rotation = Quaternion.Euler(0f,0f, angle);
+        // LookAtMouse
+        Vector3 mouse = Input.mousePosition;
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+        Vector2 offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f,0f, angle);
+
+        //Mouse Button Held Down
+        leftMouseDown = Input.GetMouseButton(0);
+        timer += Time.deltaTime;
    }
+
+   private IEnumerator ChargeTimer() {
+         Debug.Log("yo");
+         while (true) {
+             yield return new WaitForSeconds(chargeDelay);
+
+             //Checking left mouse down and incrementing charge or shooting ball
+             if (leftMouseDown)
+             {
+                if (charge >= maxCharge)
+                {
+                    charge = maxCharge;
+                }
+                else {
+                    charge++;
+                }
+                Debug.Log(charge);
+             }
+             else if (charge > 0)
+             {
+                SpawnTennisBall();
+                charge = 0;
+             }
+             else
+             {
+                 charge = 0;
+             }
+         }
+     }
 
    void Move() 
     {
@@ -45,5 +85,11 @@ public class PlayerController : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
 
         moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void SpawnTennisBall()
+    {
+        Debug.Log("ball spawned");
+        
     }
 }
